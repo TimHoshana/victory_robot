7733338769fe925147c9d310ad40d49286ffafd4
TimHoshana <tim.hoshana@gmail.com>
2026-01-30 09:16:51 +0200

    go forward. motors rest1.

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,33 +1,56 @@
+/*
+Author  : Andrea Lombardo
+Site    : https://www.lombardoandrea.com
+Source  : https://github.com/AndreaLombardo/L298N/
+
+Here you can see how to work in a common configuration without the needed of Enable pin. 
+
+Make sure your module has a jumper in place.
+
+When L298N has a jumper instead of Enable pin, the speed is always 255.
+
+Wiring schema in file "L298N - Schema_without_EN_pin.png"
+*/
+
+// Include the library
+#include <L298N.h>
 #include <Arduino.h>
-#include <array>
-#include "configs.h"
-#include "sensorse/QTR.h"
 
+// Pin definition
+const uint8_t IN1 = 5;
+const uint8_t IN2 = 17;
+const uint8_t IN3 = 18;
+const uint8_t IN4 = 19;
 
-QTR Qtr(qtrSensor, qrtMax, qrtMin, qtrLed);
+// Create one motor instance
 
-void setup()
-{
-  // configure the sensors
-  Qtr.setup();
-  Serial.begin(115200);
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
-void loop()
-{
-  // read raw sensor values
-  Qtr.lineDetaction();
-
-  Qtr.linePosition();
-
-  auto pos = Qtr.linePosition();
-
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(pos[i]);
-    Serial.print('\t');
+void motorLoop(int val, uint8_t pinIn1, uint8_t pinIn2) {
+  val = map(val, 0, 1023, -255, 255);
+  if (val > 0){
+    analogWrite(pinIn2, val);
+    digitalWrite(pinIn1, 0);
   }
-  Serial.println();
-
-  delay(200);
+  else if (val < 0){
+    analogWrite(pinIn2, 255+val);
+    digitalWrite(pinIn1, 1);
+  }
+  else {
+    digitalWrite(pinIn1, 0);
+    digitalWrite(pinIn2, 0);
+  }
 }
+
+void loop() {
+  motorLoop(600, IN1, IN2); // Move motor 1 forward at speed 100
+  motorLoop(5, IN3, IN4); // Move motor 2 backward at speed 150
+}
+
